52c760520f55453b7c984af41d104c6157eba7fb
george-kirby <georgeskirby@btinternet.com>
2022-10-10 18:24:20 +0100

    installed nodemon for hot reload

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
+
+app.use(cors({
+  origin: "http://localhost:3001"
+}))
+
+app.get('/tasks', (req, res) => {
+  res.send("you GET someee /tasks!")
+})
 
 app.post('/tasks', (req, res) => {
   res.send("you did it!")
