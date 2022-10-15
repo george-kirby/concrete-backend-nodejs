de181f9981c3c9cb93a0292bf02da2abc9eab05a
george-kirby <georgeskirby@btinternet.com>
2022-10-15 23:57:08 +0100

    add very basic login error handling

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,9 +6,17 @@ const processLoginAttempt = async (request, response) => {
     console.log("SUBMITTED USER DETAILS:", configuredRequestBody)
     console.log("REQUEST BODY:", request.body)
     // console.log("CONFIGURED REQUEST BODY:", configuredRequestBody)
+
+    // check with database
+    // returns user object { email: ..., password: ...} if credentials correct
+    // or empty array if credentials incorrect
     const databaseCheck = await User.find(configuredRequestBody)
     console.log(databaseCheck)
-    response.json(databaseCheck)
+    if (databaseCheck.length > 0) {
+        response.json(databaseCheck)
+    } else {
+        response.json({ errors: ["Login credentials failed. Please try again."] })
+    }
 
     // ERROR HANDLING
 
