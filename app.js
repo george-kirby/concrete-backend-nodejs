d7298dca83393b760dd1ebac0d4b880bece0c510
george-kirby <georgeskirby@btinternet.com>
2022-11-15 15:30:51 +0000

    add placeholder auth header check

--- app.js
+++ app.js
@@ -25,12 +25,26 @@ const taskRoutes = require('./routes/tasks')
 const loginRoutes = require('./routes/login')
 
 // middleware
-app.use(morgan("dev"))
+app.use(morgan("common"))
 app.use(bodyParser.json())
 app.use(expressValidator())
-app.use(cors({
-  origin: "http://localhost:3000"
-}))
+// app.use(function to check if request has valid authorisation header)
+// If good, set global variable currentUser
+// Else, return ?401 with error message
+app.use((request, response, next) => {
+  const authHeader = request.headers["Authorization"] || request.headers["Authorisation"] || request.headers["authorization"] || request.headers["authorisation"]
+  authHeader === "letMeIn" ?
+    next() :
+    response.status(401).json("Unauthorized")
+})
+
+const origin = "http://localhost:3000"
+const corsOptions = {
+  origin,
+  optionsSuccessStatus: 200
+}
+
+app.options(origin, cors(corsOptions))
 
 // routes
 app.use("/tasks", taskRoutes)
